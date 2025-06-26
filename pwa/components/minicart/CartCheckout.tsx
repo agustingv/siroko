6d43b27bfdb515836ff4819d52bd23ba9dbb1e99
Agustín García <contacto@agustin-garcia.es>
2025-06-26 09:07:58 +0200

    set checkout and pay funcs

--- pwa/components/minicart/CartCheckout.tsx
+++ pwa/components/minicart/CartCheckout.tsx
@@ -1,10 +1,19 @@
 import CartItem from "./CartItem"
 import { useCartStore } from "../../stores/useCartStore"
 import useFromStore from "../../hooks/useFromStore"
+import { v4 } from "uuid";
+
+
+
 
 function Cart() {
     const cart = useFromStore(useCartStore, state => state.cart)
+    const cleanCart = useCartStore(state => state.cleanCart)
 
+    const onPayClick = () => {
+
+      console.log(v4())
+    }
     let total = 0
     if (cart) {
         total = cart.reduce((acc, product) => acc + product.price * (product.quantity as number), 0)
@@ -22,7 +31,10 @@ function Cart() {
                 <span className='text-lg font-bold'>Total:</span>
                 <span className='text-xl font-bold'>${total.toFixed(2)}</span>
             </div>
-            <button>Pay</button>
+            <button type='button'
+                    className='ml-2 bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600'
+                    onClick={() => cleanCart()}
+					>Pay and Clean</button>
         </section>
     )
 }
